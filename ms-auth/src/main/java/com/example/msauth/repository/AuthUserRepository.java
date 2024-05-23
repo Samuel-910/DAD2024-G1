f3ad1523624a34b09587afa8c29d7e90d6c7e102
Samuel-910 <dasarupazfesandra@gmail.com>
2024-05-23 09:07:59 -0500

    Security

--- ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
@@ -7,7 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser,Integer> {
-
-
     Optional<AuthUser> findByUserName(String username);
 }
